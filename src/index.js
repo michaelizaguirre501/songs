5a95a5d231708642cb26e67e241857efe3f27c90
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-09 18:24:02 -0400

    link reducers to app with provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
 
 if (module.hot) {
   module.hot.accept();
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
