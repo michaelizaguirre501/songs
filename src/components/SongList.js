8175662afc7bd73e667d13ce40df2659a5f119ce
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-10-09 18:57:47 -0400

    list with buttons mapped via renderlist function

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,9 +2,23 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class SongList extends Component {
+  renderList() {
+    return this.props.songs.map((song, index) => {
+      return (
+        <div className="item" key={index}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    console.log(this.props);
-    return <div>Song list</div>;
+    console.log("~~PROPS~~", this.props);
+
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
